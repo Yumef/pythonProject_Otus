cf85e7e458256940c2440a627bda28dbf6ad101a
YuliaMef <yulia.mef@gmail.com>
2021-11-17 09:56:42 +0300

    Add new homework_03

--- homework_03.py
+++ homework_03.py
@@ -6,28 +6,54 @@ books = []
 # Прочесть список книг
 # Составить для каждой книги отельный словарь в списке с ключами title, author, genre, publisher
 with open('files/books-133064-871075.csv', newline='') as f:
-   reader = DictReader(f)
-   for row in reader:
-       books.append(row)
+    reader = DictReader(f)
+    for row in reader:
+        books.append(row)
+
+        row["publisher"] = row["Publisher"]
+        row["title"] = row["Title"]
+        row["author"] = row["Author"]
+        row["pages"] = int(row["Pages"])
+        row["genre"] = row["Genre"]
+
+        del row["Publisher"]
+        del row["Title"]
+        del row["Author"]
+        del row["Pages"]
+        del row["Genre"]
 
 
 # Проочесть список юзеров
 with open("files/users.json", "r") as f:
-   read_users = f.read()
-   users = json.loads(read_users)
+    read_users = f.read()
+    users = json.loads(read_users)
 
 
+# Убрать лишние параметры
 # Для каждого юзера создать параметр books с пустым списком для словарей из книг
-with open("files/users.json", "w") as f:
-    for user in users:
-        user['books'] = []
-    text = json.dumps(user, indent=4)  # отступы
+users_result = []
+for user in users:
+    data = {
+        "name": user["name"],
+        "gender": user["gender"],
+        "address": user["address"],
+        "age": user["age"],
+        "books": []
+        }
+
+    users_result.append(data)
+
+with open("files/result.json", "w") as f:
+    text = json.dumps(users_result, indent=4)
     f.write(text)
 
-# Циклом вписать для каждого юзера книги в books
-count_books = int(len(books) / len(users))
 
-for user in users:
+
+
+# Циклом вписать для каждого юзера книги в books
+count_books = int(len(books) / len(users_result))
+
+for user in users_result:
     for i in range(count_books):
         data = user.get('books')
         data.append(books[i])
@@ -35,12 +61,12 @@ for user in users:
 
 u = -1
 for i in range(len(books)):
-    data = users[u+1].get('books')
+    data = users_result[u+1].get('books')
     data.append(books[i])
     u += 1
 
 with open("files/result.json", "w") as f:
-    text = json.dumps(users, indent=4)  # отступы
+    text = json.dumps(users_result, indent=4)  # отступы
     f.write(text)
 
 
